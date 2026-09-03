45132ad23ae965ff965281cd7804b33ec0165bc0
NAMAN TIWARI <nt458860@gmail.com>
2026-09-03 15:55:03 +0530

    making slight changes to the intial file

---
+++ source_code/documents_loaders.py
@@ -0,0 +1,30 @@
+from langchain_community.document_loaders import DirectoryLoader,TextLoader,PyPDFLoader
+
+def docs_loader(folder):
+    Text_data=DirectoryLoader(
+        path=folder,
+        glob="**/*.txt",
+        loader_cls=TextLoader,
+        loader_kwargs={"encoding":"utf-8"},
+        show_progress=True,
+        ).load()
+
+    md_data=DirectoryLoader(
+        path=folder,
+        glob="**/*.md",
+        loader_cls=TextLoader,
+        loader_kwargs={"encoding":"utf-8"},
+        show_progress=True,
+        ).load()
+
+    pdf_data=DirectoryLoader(
+        path=folder,
+        glob="**/*.pdf",
+        loader_cls=PyPDFLoader,
+        show_progress=True,
+        ).load()
+
+    docs= md_data+pdf_data+Text_data
+    return docs
+
+
